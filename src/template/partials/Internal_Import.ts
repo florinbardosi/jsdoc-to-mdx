bb7efb0285e18a067e066299f54a19eebbe828ea
곽대현 <naviialto@gmail.com>
2024-09-02 09:37:12 +0900

    bugfix

--- src/template/partials/Internal_Import.ts
+++ src/template/partials/Internal_Import.ts
@@ -6,7 +6,7 @@ import DocumentParams from "../../types/DocumentParams";
 /*
 "customTags": [
     {
-        tag: 'internal444',
+        tag: 'internal_import',
         value: "import Tabs from '@theme/Tabs';\nimport TabItem from '@theme/TabItem';"
     }
 ]
@@ -14,6 +14,6 @@ import DocumentParams from "../../types/DocumentParams";
 export default (classData: DocumentedClass) => {
     return classData.customTags
         ?.filter(tag => tag.tag === 'internal_import')
-        .map(tag => tag)
+        .map(tag => tag.value)
         .join("\n") ?? '';
 }
